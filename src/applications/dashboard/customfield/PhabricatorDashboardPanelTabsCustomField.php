86c56cd38a690b8175849cf283ef2e84cef28b1c
Ankur Goyal <ankur@memsql.com>
2015-02-08 13:46:10 -0800

    Increase Number of Tabs to 20 in Dashboard

--- src/applications/dashboard/customfield/PhabricatorDashboardPanelTabsCustomField.php
+++ src/applications/dashboard/customfield/PhabricatorDashboardPanelTabsCustomField.php
@@ -55,7 +55,7 @@ final class PhabricatorDashboardPanelTabsCustomField
     }
 
     $out = array();
-    for ($ii = 1; $ii <= 6; $ii++) {
+    for ($ii = 1; $ii <= 20; $ii++) {
       $tab = idx($value, ($ii - 1), array());
       $out[] = id(new AphrontFormTextControl())
         ->setName($this->getFieldKey().'_name[]')
