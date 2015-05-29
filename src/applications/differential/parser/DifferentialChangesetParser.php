cd94b587b27f063e805268dde1c58f61c6571ec8
Carl Sverre <accounts@carlsverre.com>
2015-05-29 16:02:45 -0700

    switching highlight limit to 2 MB

--- src/applications/differential/parser/DifferentialChangesetParser.php
+++ src/applications/differential/parser/DifferentialChangesetParser.php
@@ -2,7 +2,7 @@
 
 final class DifferentialChangesetParser extends Phobject {
 
-  const HIGHLIGHT_BYTE_LIMIT = 262144;
+  const HIGHLIGHT_BYTE_LIMIT = 2097152;
 
   protected $visible      = array();
   protected $new          = array();
